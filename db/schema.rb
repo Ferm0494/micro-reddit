56670b47f3b9b00a2c8054b87bda00928acfe74a
Muhammad Arslan <thearslan380@gmail.com>
2020-06-09 10:20:33 -0700

    post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_163025) do
+ActiveRecord::Schema.define(version: 2020_06_09_171337) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
