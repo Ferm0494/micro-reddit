86372c1342e23db02a9013a4d6cb5f5a87f87a35
Ferm0494 <ferm0494@gmail.com>
2020-06-09 13:51:03 -0600

    Fixing Linter errors rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    validates :email, :password, :name, presence: true, length: {minimum: 6, maximum: 50 }
+  has_many :posts
+  has_many :comments
+  validates :email, :password, :name, presence: true, length: { minimum: 6, maximum: 50 }
 end
